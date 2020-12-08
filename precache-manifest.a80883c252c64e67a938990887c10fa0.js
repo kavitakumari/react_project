0cd52f30570d6665778f88bd12539b6e5b74ec0e
kavitakumari <42225167+kavitakumari@users.noreply.github.com>
2020-12-07 21:06:13 -0800

    Updates

---
+++ precache-manifest.a80883c252c64e67a938990887c10fa0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78efc0e0fec5182cc8f42719afb267ea",
+    "url": "/react_project/index.html"
+  },
+  {
+    "revision": "5b454746cc46218ec52f",
+    "url": "/react_project/static/css/main.45d8370c.chunk.css"
+  },
+  {
+    "revision": "5043f9b5ded148a9e4ea",
+    "url": "/react_project/static/js/2.2aaa479d.chunk.js"
+  },
+  {
+    "revision": "5b454746cc46218ec52f",
+    "url": "/react_project/static/js/main.eb862e16.chunk.js"
+  },
+  {
+    "revision": "62d0d6f3c8c69af4455f",
+    "url": "/react_project/static/js/runtime~main.e1d542ef.js"
+  }
+]);
